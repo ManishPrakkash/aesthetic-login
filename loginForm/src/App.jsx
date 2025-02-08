7bf7ce32695872910e1eb55143f074d779bdaee4
ManishPrakkash <manishprakkash@gmail.com>
2025-02-08 10:22:05 +0000

    simple login structure created

--- loginForm/src/App.jsx
+++ loginForm/src/App.jsx
@@ -7,7 +7,26 @@ function App() {
 
   return (
     <>
-    
+    <div className="form">
+    <form action="/">
+      <h1>login</h1>
+
+      <div className="input-box">
+        <input type="text" placeholder='Username' />
+      </div>
+      <div className="input-box">
+        <input type="text" placeholder='Password' />
+      </div>
+      <div className="forgot"><label><input type="checkbox"/>Remember me</label>
+      <a href="#">Forgot password</a>
+      </div>
+
+      <button>Login</button>
+      <div className="register">
+        <p>Don't have an account?<a href="#">Register</a></p>
+      </div>
+   </form>
+    </div>
     </>
   )
 }
